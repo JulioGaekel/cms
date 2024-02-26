69224af50d745d05aa7a70368a792c7774d7c2af
Julio Gaekel <jriveragaekel@gmail.com>
2024-02-26 21:38:45 +0100

    Add queries to display posts from db
    - SQL queries to select all information and then display it properly in the CMS template automatically.

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<?php global $connection ?>
 <?php include "includes/header.php"?>
 
     <!-- Navigation -->
@@ -11,26 +12,43 @@
             <!-- Blog Entries Column -->
             <div class="col-md-8">
 
-                <h1 class="page-header">
-                    Page Heading
-                    <small>Secondary Text</small>
-                </h1>
+                <?php
+                    $query = "SELECT * FROM posts";
+                    $selectAllPostsQuery = mysqli_query($connection, $query);
 
-                <!-- First Blog Post -->
-                <h2>
-                    <a href="#">Blog Post Title</a>
-                </h2>
-                <p class="lead">
-                    by <a href="index.php">Start Bootstrap</a>
-                </p>
-                <p><span class="glyphicon glyphicon-time"></span> Posted on August 28, 2013 at 10:00 PM</p>
-                <hr>
-                <img class="img-responsive" src="http://placehold.it/900x300" alt="">
-                <hr>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, veritatis, tempora, necessitatibus inventore nisi quam quia repellat ut tempore laborum possimus eum dicta id animi corrupti debitis ipsum officiis rerum.</p>
-                <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+                    while($row = mysqli_fetch_assoc($selectAllPostsQuery)) {
+                        $postTitle = $row['post_title'];
+                        $postAuthor = $row['post_author'];
+                        $postDate = $row['post_date'];
+                        $postImg = $row['post_img'];
+                        $postContent = $row['post_content'];
 
-                <hr>
+                ?>
+                    <h1 class="page-header">
+                        Page Heading
+                        <small>Secondary Text</small>
+                    </h1>
+
+                    <!-- First Blog Post -->
+                    <h2>
+                        <a href="#"><?php echo $postTitle ?></a>
+                    </h2>
+                    <p class="lead">
+                        by <a href="index.php"><?php echo $postAuthor ?></a>
+                    </p>
+                    <p><span class="glyphicon glyphicon-time"></span><?php echo $postDate ?></p>
+                    <hr>
+                    <img class="img-responsive" src="http://placehold.it/900x300" alt="">
+                    <hr>
+                    <p><?php echo $postContent ?></p>
+                    <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+
+                    <hr>
+
+
+                <?php } ?>
+
+
 
             </div>
 
